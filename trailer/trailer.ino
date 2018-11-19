c6a1e5f594a61ef30682915c3627695d7b071195
CoderDoctorLee <496347267@qq.com>
2018-11-19 16:11:44 +0800

    更新

--- trailer/trailer.ino
+++ trailer/trailer.ino
@@ -33,11 +33,13 @@ byte vibrate = 0;
 int initialAngle = 90; //舵机初始角度
 int max_Angle = 130; //最大角度
 int min_Angle = 60; //最小角度
-int per_Angle = 3; //每次角度变化量
+int per_Angle = 5 ; //每次角度变化量
 int speed_range[3] = {85, 170, 255}; //三个前进档位速度上限
+int current_speed = 120; // 当前速度《120映射为0》
 int reverse_max_speed = 85; //倒挡最大速度
 int current_level = 0; //初始档位
 static unsigned long last_time = millis(); //上次档位变化的时间
+static unsigned long change_speed_timer = millis();
 
 int temp_x = 127; //记录上一次的x
 int temp_y = 128; //记录上一次的y
@@ -71,19 +73,18 @@ void setup() {
   pinMode(MotorD_IN1, OUTPUT);
   pinMode(MotorD_IN2, OUTPUT);
   error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
-  type = ps2x.readType();
+  //  type = ps2x.readType();
 
   mp3.begin(Serial1);
-  mp3.volume(20);  //设置音量  0 - 30
+  mp3.volume(30);  //设置音量  0 - 30
   mp3.disableLoop(); //取消循环
 }
 
 void loop() {
-
   //判断汽车是否已经启动
   if (start_flag)
   {
-    if (millis() - music_timer >= 4500)
+    if (millis() - music_timer >= 5000)
     {
       mp3.loop(2);
       music_timer = millis();
@@ -112,16 +113,16 @@ void loop() {
 
     vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
 
-    Serial.print("Stick Values:");
-    Serial.print(ps2x.Analog(PSS_LX), DEC); //Left stick, Y axis. Other options: LX, RY, RX
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_LY), DEC);
-    Serial.print(",");
-    Serial.print(initialAngle);
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_RY), DEC);
-    Serial.print(",");
-    Serial.println(ps2x.Analog(PSS_RX), DEC);
+    //    Serial.print("Stick Values:");
+    //    Serial.print(ps2x.Analog(PSS_LX), DEC); //Left stick, Y axis. Other options: LX, RY, RX
+    //    Serial.print(",");
+    //    Serial.print(ps2x.Analog(PSS_LY), DEC);
+    //    Serial.print(",");
+    //    Serial.print(initialAngle);
+    //    Serial.print(",");
+    //    Serial.print(ps2x.Analog(PSS_RY), DEC);
+    //    Serial.print(",");
+    //    Serial.println(ps2x.Analog(PSS_RX), DEC);
 
     if (ps2x.Button(PSB_L3))
       Serial.println("L3 pressed");
@@ -133,10 +134,11 @@ void loop() {
       {
         Serial.println("L1 pressed");
         //启动汽车:
-        if (!start_flag)
+        if (!start_flag && stop_flag)
         {
           mp3.play(1);
           start_flag = true; //启动，设置为true
+          stop_flag = false;
         }
         else
         {
@@ -150,6 +152,8 @@ void loop() {
     if (ps2x.Button(PSB_R1))
     {
       Serial.println("R1 pressed");
+      //急刹车
+      brakes();
     }
     if (ps2x.Button(PSB_L2))
     {
@@ -203,22 +207,23 @@ void loop() {
 
     if (LX == 128 && LY == 127 && initialAngle != 90)
     {
-      initialAngle < 90 ? (initialAngle += 2 * per_Angle) : (initialAngle -= 2 * per_Angle);
+      initialAngle < 90 ? (initialAngle += per_Angle) : (initialAngle -= per_Angle);
+      initialAngle = initialAngle == 90 ? 90 : initialAngle;
       barrel1.write(initialAngle);
       barrel2.write(initialAngle);
     }
 
     swerve(LX, LY); // 实时转向
 
-    if (RY == 127)
+    if (RY > 120 && RY < 135)
     {
       tank_stop(); //停止
     }
-    if (RY < 127)
+    if (RY <= 120)
     {
       advance(RY); //前进
     }
-    else if (RY > 127)
+    else if (RY >= 135)
     {
       retreat(RY); //后退
     }
@@ -228,57 +233,121 @@ void loop() {
 
 //前进
 void advance(int speed) {
+  Serial.print("上一次速度:");
+  Serial.println(current_speed);
   stop_flag = false;
-  //Motor_A
-  analogWrite(MotorA_IN1, map(speed, 126, 0, 0, speed_range[current_level]));
-  digitalWrite(MotorA_IN2, LOW);
-  analogWrite(MotorC_IN1, map(speed, 126, 0, 0, speed_range[current_level]));
-  digitalWrite(MotorC_IN2, LOW);
-  //Motor_B
-  digitalWrite(MotorB_IN1, LOW);
-  analogWrite(MotorB_IN2, map(speed, 126, 0, 0, speed_range[current_level]));
-  digitalWrite(MotorD_IN1, LOW);
-  analogWrite(MotorD_IN2, map(speed, 126, 0, 0, speed_range[current_level]));
+  if (speed > current_speed) {
+    //减速
+    if (millis() - change_speed_timer > 200) {
+      current_speed += 3;
+      current_speed = current_speed >= 120 ? 120 : current_speed;
+      Serial.print("减速，当前速度:");
+      Serial.println(current_speed);
+      //Motor_A
+      analogWrite(MotorA_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorA_IN2, LOW);
+      analogWrite(MotorC_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorC_IN2, LOW);
+      //Motor_B
+      digitalWrite(MotorB_IN1, LOW);
+      analogWrite(MotorB_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorD_IN1, LOW);
+      analogWrite(MotorD_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    }
+  }
+  if (speed < current_speed) {
+    //加速
+    if (millis() - change_speed_timer > 200) {
+      current_speed -= 3;
+      current_speed = current_speed <= 0 ? 0 : current_speed;
+      Serial.print("加速，当前速度:");
+      Serial.println(current_speed);
+      //Motor_A
+      analogWrite(MotorA_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorA_IN2, LOW);
+      analogWrite(MotorC_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorC_IN2, LOW);
+      //Motor_B
+      digitalWrite(MotorB_IN1, LOW);
+      analogWrite(MotorB_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+      digitalWrite(MotorD_IN1, LOW);
+      analogWrite(MotorD_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    }
+  }
 }
 
 //后退
 void retreat(int speed) {
   stop_flag = false;
   //Motor_A
-  analogWrite(MotorA_IN1, map(speed, 128, 255, 150, 80));
+  analogWrite(MotorA_IN1, map(speed, 135, 255, 150, 80));
   digitalWrite(MotorA_IN2, HIGH);
-  analogWrite(MotorC_IN1, map(speed, 128, 255, 150, 80));
+  analogWrite(MotorC_IN1, map(speed, 135, 255, 150, 80));
   digitalWrite(MotorC_IN2, HIGH);
   //Motor_B
   digitalWrite(MotorB_IN1, HIGH);
-  analogWrite(MotorB_IN2, map(speed, 128, 255, 150, 80));
+  analogWrite(MotorB_IN2, map(speed, 135, 255, 150, 80));
   digitalWrite(MotorD_IN1, HIGH);
-  analogWrite(MotorD_IN2, map(speed, 128, 255, 150, 80));
-
-  Serial.print("后退速度");
-  Serial.println(map(speed, 128, 255, 150, 80));
+  analogWrite(MotorD_IN2, map(speed, 135, 255, 150, 80));
 }
 
-//停止
+//减速缓慢停止
 void tank_stop() {
+  if (millis() - change_speed_timer > 200 && current_speed != 120)
+  {
+    current_speed += 5;
+    current_speed = current_speed >= 120 ? 120 : current_speed;
+    //Motor_A
+    analogWrite(MotorA_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    digitalWrite(MotorA_IN2, LOW);
+    analogWrite(MotorC_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    digitalWrite(MotorC_IN2, LOW);
+    //Motor_B
+    digitalWrite(MotorB_IN1, LOW);
+    analogWrite(MotorB_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    digitalWrite(MotorD_IN1, LOW);
+    analogWrite(MotorD_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+    if (!stop_flag && start_flag && current_speed == 120)
+    {
+      mp3.play(3);
+      stop_flag = true;
+    }
+  }
+
+}
+
+//急停
+void brakes()
+{
   if (!stop_flag && start_flag)
   {
-      mp3.play(3);
-//      mp3.disableLoop();
-      stop_flag = true;
+    mp3.play(3);
+    stop_flag = true;
   }
+  current_speed = 120;
   //Motor_A
-  analogWrite(MotorA_IN1, 0);
+  analogWrite(MotorA_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
   digitalWrite(MotorA_IN2, LOW);
-  //Motor_C
-  analogWrite(MotorC_IN1, 0);
+  analogWrite(MotorC_IN1, map(current_speed, 120, 0, 0, speed_range[current_level]));
   digitalWrite(MotorC_IN2, LOW);
   //Motor_B
   digitalWrite(MotorB_IN1, LOW);
-  analogWrite(MotorB_IN2, 0);
-  //Motor_D
+  analogWrite(MotorB_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
   digitalWrite(MotorD_IN1, LOW);
-  analogWrite(MotorD_IN2, 0);
+  analogWrite(MotorD_IN2, map(current_speed, 120, 0, 0, speed_range[current_level]));
+
+  //  //Motor_A
+  //  analogWrite(MotorA_IN1, 0);
+  //  digitalWrite(MotorA_IN2, LOW);
+  //  //Motor_C
+  //  analogWrite(MotorC_IN1, 0);
+  //  digitalWrite(MotorC_IN2, LOW);
+  //  //Motor_B
+  //  digitalWrite(MotorB_IN1, LOW);
+  //  analogWrite(MotorB_IN2, 0);
+  //  //Motor_D
+  //  digitalWrite(MotorD_IN1, LOW);
+  //  analogWrite(MotorD_IN2, 0);
 }
 
 //计算转向
