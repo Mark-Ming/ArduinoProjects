798c69244141638515ab6ee1b425fb37c56bc06a
CoderDoctorLee <496347267@qq.com>
2018-11-30 21:44:27 +0800

    日常提交

---
+++ Tank_A/Tank_A.ino
@@ -0,0 +1,218 @@
+#include <Servo.h>
+
+//后马达 A(右) / B(左)
+#define MotorA_IN1 7
+#define MotorA_IN2 6
+#define MotorB_IN1 5
+#define MotorB_IN2 4
+
+/* 马达相关变量 */
+bool start_flag = false;              //卡车现在是否已经启动标识
+bool stop_flag = true;                //卡车现在是否已经停止标识
+int min_forward_value = 120;          //对应前进时最小速度(遥杆值)
+int max_forward_value = 0;            //对应前进时最大速度(遥杆值)
+int min_back_value = 150;             //对应后退时最小速度(遥杆值)
+int max_back_value = 255;             //对应后退时最大速度(遥杆值)
+int speed_range[3] = {120, 170, 255}; //三个前进档各自速度上限(遥杆值)
+int back_speed_limit = 255;           //倒车最大速度[0-255]
+int current_level = 2;                //档位 [初始为三档]
+int current_speed = 120;              //前进速度(遥杆值) [ 120映射为0 ] => [120 ~ 0]
+int turn_left_speed = 0;              //左转速度
+int turn_right_speed = 0;             //右转速度
+
+
+/* 舵机相关设置 */
+Servo barrel1;                    //炮台舵机
+int initialAngle = 100;            //炮台舵机初始角度
+int max_Angle = 180;    //转向最大角度
+int min_Angle = 10;     //转向最小角度
+int per_Angle = 2;      //每次舵机角度变化量
+static unsigned long last_back_change_timer = millis();      //上次升降状态变化的时间
+static unsigned long last_back_change_step_timer = millis(); //每次变化时间间隔
+
+
+/* 遥控部分 */
+#define ROCKER_LEFT  16    //x向左
+#define ROCKER_RIGHT 32    //x向右
+#define ROCKER_UP    48    //y向上
+#define ROCKER_DOWN  64    //y向下
+
+#define ROCKER_ADVANCE 80   //y向上
+#define ROCKER_RETREAT 96   //y向下
+#define ROCKER_LEFT_2  128  //x向左
+#define ROCKER_RETREAT 144  //x向右
+
+#define BUTTON 112         // 按钮情况
+#define ROTATE_TO_LEFT  4  //坦克炮台左旋转
+#define ROTATE_TO_RIGHT 5  //坦克炮台右旋转
+#define TANK_FIRE       6  //坦克开火
+
+#define HIGH_FOUR 240        //取出高四位key部分使用  按位与
+#define LOW_FOUR  15         //取出低四位data部分使用  按位与
+
+int key = 0;
+int value = 0;
+int value_button = 0;
+
+int LX = 127; //左摇杆x初始值   控制左右转向
+int RY = 127; //右摇杆y初始值   控制前进后退
+
+unsigned char arr[4] = {};     // 遥控器信息原始数组
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);              //遥控器使用
+  Serial.begin(115200);
+  barrel1.attach(22);               //炮台舵机
+  barrel1.write(initialAngle);
+  //设置模式开启
+  //  pinMode(20, OUTPUT);
+  //  analogWrite(20, LOW);
+}
+
+void loop() {
+  //    Serial1.print("AT+RX\r\n");
+  //    String str = Serial1.readString();
+  //    Serial.println(str);
+
+  Serial1.readBytes(arr, 4);       //读取遥控器信息
+  handleData(arr);                 //处理遥控器信息
+
+  //    Serial.print(arr[0]);
+  //    Serial.print("    ");
+  //    Serial.print(arr[1]);
+  //    Serial.print("    ");
+  //    Serial.print(arr[2]);
+  //    Serial.print("    ");
+  //    Serial.print(arr[3]);
+  //    Serial.println("    ");
+
+  Serial.print("LX : ");
+  Serial.print(LX);
+  Serial.print("RY : ");
+  Serial.println(RY);
+  //  Serial.print("按钮 : ");
+  //  Serial.println(value_button);
+
+  if (value_button == TANK_FIRE)
+  {
+    Serial.println("开火");
+  }
+
+  if (value_button == ROTATE_TO_LEFT)
+  {
+    //    Serial.println("炮台左转");
+    initialAngle += per_Angle;
+    initialAngle = initialAngle >= max_Angle ? max_Angle : initialAngle;
+    barrel1.write(initialAngle);
+    Serial.print("当前角度:");
+    Serial.println(initialAngle);
+  }
+  if (value_button == ROTATE_TO_RIGHT)
+  {
+    //    Serial.println("炮台右转");
+    initialAngle -= per_Angle;
+    initialAngle = initialAngle <= min_Angle ? min_Angle : initialAngle;
+    barrel1.write(initialAngle);
+  }
+
+  if (RY > 120 && RY < 150)
+  {
+    trailStop();
+  }
+  if (RY <= 120)
+  {
+    advance(RY); //前进
+  }
+  else if (RY >= 150)
+  {
+    retreat(RY);//后退
+  }
+  delay(40);
+}
+
+//卡车前进函数 ---- 参数 : des_speed 当前遥杆传进来的速度
+void advance(int des_speed)
+{
+  Serial.print("前进");
+  motorForward(des_speed);
+}
+
+//卡车后退函数 ---- 参数 : des_speed 当前遥杆传进来的速度
+void retreat(int des_speed)
+{
+  motorBackward(des_speed);
+}
+
+//卡车停止函数(缓慢停止)
+void trailStop()
+{
+  //Motor_A
+  analogWrite(MotorA_IN1, map(min_forward_value, min_forward_value, max_forward_value, 0, speed_range[current_level]));
+  digitalWrite(MotorA_IN2, LOW);
+  //Motor_B
+  digitalWrite(MotorB_IN1, LOW);
+  analogWrite(MotorB_IN2, map(min_forward_value, min_forward_value, max_forward_value, 0, speed_range[current_level]));
+}
+
+//马达前转函数 ---- 参数 : des_speed 目标速度
+void motorForward(int des_speed)
+{
+  des_speed = des_speed >= min_forward_value ? min_forward_value : des_speed;
+  des_speed = des_speed <= max_forward_value ? max_forward_value : des_speed;
+  turn_left_speed = 0;
+  turn_right_speed = 0;
+  //判断转向情况  LX
+  if (LX <= 126 && LX >= 0)
+  {
+    //左转   左轮马达B减速
+    turn_left_speed = map(LX, 126, 0, 0, 100);
+  }
+  else if (LX >= 128 && LX <= 255)
+  {
+    //右转   右轮马达A减速
+    turn_right_speed = map(LX, 128, 255, 0, 100);
+  }
+
+  //Motor_A
+  analogWrite(MotorA_IN1, map(des_speed + turn_left_speed, min_forward_value, max_forward_value, 0, speed_range[current_level]));
+  digitalWrite(MotorA_IN2, LOW);
+  //Motor_B
+  digitalWrite(MotorB_IN1, LOW);
+  analogWrite(MotorB_IN2, map(des_speed + turn_right_speed , min_forward_value, max_forward_value, 0, speed_range[current_level]));
+}
+
+//马达后转函数 ---- 参数 : des_speed 目标速度
+void motorBackward(int des_speed)
+{
+  des_speed = des_speed >= max_back_value ? max_back_value : des_speed;
+  des_speed = des_speed <= min_back_value ? min_back_value : des_speed;
+  //Motor_A
+  digitalWrite(MotorA_IN1, LOW);
+  analogWrite(MotorA_IN2, map(des_speed, min_back_value, max_back_value, 0, back_speed_limit));
+  //Motor_B
+  analogWrite(MotorB_IN1, map(des_speed, min_back_value, max_back_value, 0, back_speed_limit));
+  digitalWrite(MotorB_IN2, LOW);
+}
+
+//处理接受到的数据包
+void handleData(unsigned char *arr)
+{
+  LX = 127;
+  RY = 127;
+  value_button = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    key = arr[i] & HIGH_FOUR;
+    value = arr[i] & LOW_FOUR;
+    switch (key) {
+      case ROCKER_LEFT:  LX = map(value, 15, 1, 128, 255); break;  //摇杆位置旋转了180度
+      case ROCKER_RIGHT: LX = map(value, 1, 15, 126, 0); break;    //摇杆位置旋转了180度
+      //      case ROCKER_UP:  LY = map(value, 15, 1, 126, 0); break;
+      //      case ROCKER_DOWN: LY = map(value, 1, 15, 128, 255); break;
+      case ROCKER_ADVANCE:  RY = map(value, 15, 1, 126, 0); break;
+      case ROCKER_RETREAT: RY = map(value, 1, 15, 128, 255); break;
+      case BUTTON: value_button = value; break;
+    }
+  }
+}
--- remote_control_for_tankA/remote_control_for_tankA.ino
+++ remote_control_for_tankA/remote_control_for_tankA.ino
@@ -1,9 +1,164 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <CBButton.h>
 
+/* 左侧遥杆 */
+#define ROCKER_LEFT  16     //x向左
+#define ROCKER_RIGHT 32     //x向右
+#define ROCKER_UP    48     //y向上
+#define ROCKER_DOWN  64     //y向下
+
+/* 右侧遥杆 */
+#define ROCKER_ADVANCE 80   //y向上
+#define ROCKER_RETREAT 96   //y向下
+#define ROCKER_LEFT_2  128  //x向左
+#define ROCKER_RETREAT 144  //x向右
+
+/* 按钮头部均是112，Data部分1-15对应不同功能 */
+#define BUTTON 112         //除了遥杆的按钮情况
+#define ROTATE_TO_LEFT  4  //坦克炮台左旋转
+#define ROTATE_TO_RIGHT 5  //坦克炮台右旋转
+#define TANK_FIRE       6  //坦克开火
+
+//定义控制按钮
+//CBButton rotate_left(3);    //左侧(blue)按钮
+//CBButton fire(2);           //左侧上(red)按钮
+//CBButton rotate_right(4);   //右侧(yellow)按钮
+
+CBButton fire(2);       //左侧上(red)按钮
+int rotate_left = 0;    //左侧(blue)按钮   需要响应长按
+int rotate_right = 0;   //右侧(yellow)按钮 需要响应长按
+
+unsigned char arr[4] = {};
+int key = 0;
+int buttonValue = 0;   // 按钮的值
+int x_direction = 0;   // 转向遥杆x轴方位  0 静止、-1 左、1 右
+int y_direction = 0;   // 转向遥杆y轴方位  0 静止、-1 上、1 下
+int r_y_direction = 0; // 前进遥杆y轴方位  0 静止、-1 上、1 下
+
+int LX = 0;     //方向遥杆x值
+int LY = 0;     //方向遥杆y值
+int RX = 0;     //前进遥杆x值
+int RY = 0;     //前进遥杆y值
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600);
+  //设置模式开启
+  //  pinMode(20, OUTPUT);
+  //  digitalWrite(20, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  LX = analogRead(A0);   // 转向遥杆x轴
+//  LY = analogRead(A1);   // 转向遥杆y轴
+//  RX = analogRead(A2);  // 前进遥杆y轴
+  RY = analogRead(A3);  // 前进遥杆y轴
+
+  x_direction = judgeDirection(LX);
+  r_y_direction = judgeDirection(RY);
+
+  //按钮值读取 默认为1，按下为0
+  rotate_left = digitalRead(3);
+  rotate_right = digitalRead(4);
+
+//  Serial.print("LX: ");
+//  Serial.print(getRockerValue(LX));
+//
+//  Serial.print("LY: ");
+//  Serial.print(getRockerValue(LY));
+//
+//  Serial.print("RX: ");
+//  Serial.print(getRockerValue(RX));
+//
+//  Serial.print("RY: ");
+//  Serial.println(getRockerValue(RY));
+
+  switch (x_direction) {
+    case -1: key = ROCKER_LEFT; break;
+    case  0: key = 0; break;
+    case  1: key = ROCKER_RIGHT; break;
+  }
+  //将x信息存入arr[0]
+  arr[0] = key | getRockerValue(LX);  // 高四位为key,低四位为value
+
+//  switch (y_direction) {
+//    case -1: key = ROCKER_UP; break;
+//    case  0: key = 0; break;
+//    case  1: key = ROCKER_DOWN; break;
+//  }
+  //将y信息存入arr[1]
+  //arr[1] = key | getRockerValue(LY);  // 高四位为key,低四位为value
+  arr[1] = 0;  //暂时不使用
+
+  switch (r_y_direction) {
+    case -1: key = ROCKER_ADVANCE; break;
+    case  0: key = 0; break;
+    case  1: key = ROCKER_RETREAT; break;
+  }
+  //将ry信息存入arr[2]
+  arr[2] = key | getRockerValue(RY);  // 高四位为key,低四位为value
+
+  //----------------------------按钮部分(低四位0-15代表情况不同, 0值保留)-------------------------------//
+  //将按钮信息存入arr[3]   key 统一为 button
+  key = BUTTON;
+  buttonValue = 0; //每次重新设置为0
+  if (rotate_left == 0)
+  {
+    Serial.println("点击向左旋转");
+    buttonValue = ROTATE_TO_LEFT;
+  }
+  if (fire.keyDown())
+  {
+    Serial.println("点击了开火");
+    buttonValue = TANK_FIRE;
+  }
+  if (rotate_right == 0)
+  {
+    Serial.println("点击向右旋转");
+    buttonValue = ROTATE_TO_RIGHT;
+  }
+  arr[3] = key | buttonValue;  // 高四位为key,低四位为value
+
+  //发送数据
+  Serial1.write(arr, 4);
+  delay(40);
+}
+
+//整理遥杆数值
+int getRockerValue(int value)
+{
+  value = value < 40 ? 40 : value;
+  value = value > 990 ? 990 : value;
+  if (value > 450 && value < 550)
+  {
+    return 0;
+  }
+  else if (value <= 450)    // x,y前半部分
+  {
+    return map(value, 450, 40, 15, 1);
+  }
+  else if (value >= 550)    // x,y后半部分
+  {
+    return map(value, 550, 990, 1, 15);
+  }
+}
+
+//判断某个端口号对应轴的方向
+int judgeDirection(int value)
+{
+  value = value < 40 ? 40 : value;
+  value = value > 990 ? 990 : value;
+  if (value <= 450)
+  {
+    return -1;
+  }
+  else if (value > 450 && value < 550)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
 }
